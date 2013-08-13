0e1bd64be6ee55c3c11872076da8782bda42a762
vaidyasm <vaidyasm@gmail.com>
2013-08-13 22:17:25 +0545

    Cleanup master. Repo for integrated application.

--- app/models/voice_mail_info.rb
+++
@@ -1,4 +0,0 @@
-class VoiceMailInfo < ActiveRecord::Base
-  attr_accessible :callerid, :origdate, :filename
-  belongs_to :transcribe
-end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Vawhack::Application.config.secret_token = '140c74bf75cc6659fb6d4a7af92ed5a1adeb10c0fb5e37de74a9e354ef2d0e5cdd076ee84c7e8e5ee5903c12b909540f62473f8d2e277753d304a5b53dad767b'
